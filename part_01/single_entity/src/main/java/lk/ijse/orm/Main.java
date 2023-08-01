852e2b0e6720523a5c82f471f24e5ebc8650c5c0
viniduminsara <viniduminsara@gmail.com>
2023-08-01 14:20:35 +0530

    complete save book in single_entity

--- part_01/single_entity/src/main/java/lk/ijse/orm/Main.java
+++ part_01/single_entity/src/main/java/lk/ijse/orm/Main.java
@@ -1,15 +1,100 @@
 package lk.ijse.orm;
 
+import lk.ijse.orm.entity.Book;
 import lk.ijse.orm.util.FactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) {
+
+        System.out.println("CRUD operation to single entity");
+        System.out.println("===============================");
+        System.out.println("[1] Save book");
+        System.out.println("[2] Update book");
+        System.out.println("[3] Delete book");
+        System.out.println("[4] Search book");
+
+        try {
+            System.out.print("Enter your option: ");
+            int age = Integer.parseInt(reader.readLine()); // Read an integer input from the terminal
+
+            switch (age){
+                case 1:
+                    saveBook();
+                    break;
+                case 2:
+                    updateBook();
+                    break;
+                case 3:
+                    deleteBook();
+                    break;
+                case 4:
+                    searchBook();
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    System.exit(0);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+        }
+    }
+
+    private static void searchBook() {
+    }
+
+    private static void deleteBook() {
+    }
+
+    private static void updateBook() {
+    }
+
+    private static void saveBook() {
+        System.out.println("Save Book");
+        System.out.println("=========");
+        String bookId = null;
+        String bookName = null;
+        double price = 0;
+
+        try {
+            System.out.println("Enter the book id : ");
+            bookId = reader.readLine();
+
+            System.out.println("Enter the book name : ");
+            bookName = reader.readLine();
+
+            System.out.println("Enter the book price : ");
+            price = Double.parseDouble(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid price.");
+        }
+
+        Book book = new Book(bookId,bookName,price);
+
         Session session = FactoryConfiguration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
-        transaction.commit();
-        session.close();
+        try {
+            session.persist(book);
+            transaction.commit();
+            System.out.println("Book saved successfully!");
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println("Failed to save the book. Error: " + e.getMessage());
+        } finally {
+            session.close();
+        }
     }
 }
